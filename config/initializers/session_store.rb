4ccac991fc68d22ebc2d66c361731e7e419950ff
Joaquin Perez <joaquin@gorandev.com.ar>
2011-05-06 14:46:07 -0300

    Commit inicial

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Gardens::Application.config.session_store :cookie_store, :key => '_gardens_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Gardens::Application.config.session_store :active_record_store
