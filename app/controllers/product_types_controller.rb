9cfe0caa5d6cbdf761077fe91ce37a1217d1bb48
Joaquin Perez <joaquin@gorandev.com.ar>
2011-05-09 16:40:46 -0300

    Now the ProductType controller, on show, returns all PropertyValues for each Property

--- app/controllers/product_types_controller.rb
+++ app/controllers/product_types_controller.rb
@@ -5,6 +5,13 @@ class ProductTypesController < ApplicationController
       line = Hash.new
       line[:name] = p.name
       line[:id] = p.id
+      line[:possible_values] = Array.new
+      p.property_values.each do |pv| 
+        line_pv = Hash.new
+        line_pv[:id] = pv.id
+        line_pv[:value] = pv.value
+        line[:possible_values].push(line_pv)
+      end
       properties.push(line) 
     end
     render :json => properties
