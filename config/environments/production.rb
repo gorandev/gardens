d9ac23b56b91a8a2ce5317e026a112f018da5d73
Joaquin Perez <joaquin@gorandev.com.ar>
2012-05-03 16:16:56 -0300

    Corrected typo.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Gardens::Application.configure do
   # config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  config.action_controller.asset_host = "http://s3.amazonaws.com/idashboard_assets/"
+  config.action_controller.asset_host = "http://s3.amazonaws.com/idashboard_assets"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
