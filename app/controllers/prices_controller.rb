a1a21306d615dc3d4751f10cc5b19be7050cf39b
Joaquin Perez <joaquin@gorandev.com.ar>
2012-09-14 09:35:55 -0300

    Enabling MongoDB again, now that the DB is loaded.

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -70,19 +70,19 @@ class PricesController < ApplicationController
       return render :json => { :errors => { :price => "no search parameters" } }, :status => 400
     end
 
-    # if params.slice(:currency, :product, :date_from, :no_limit).size == 4
-    #   fecha_inicial = Date.parse(params[:date_from])
-    #   fecha_final = (params.has_key?(:date_to)) ? Date.parse(params[:date_to]) : Time.now.to_date
+    if params.slice(:currency, :product, :date_from, :no_limit).size == 4
+      fecha_inicial = Date.parse(params[:date_from])
+      fecha_final = (params.has_key?(:date_to)) ? Date.parse(params[:date_to]) : Time.now.to_date
 
-    #   unless params[:product].is_a?Array
-    #     params[:product] = params[:product].split(',')
-    #   end
+      unless params[:product].is_a?Array
+        params[:product] = params[:product].split(',')
+      end
 
-    #   @pricepoints = PricePoint.any_in(:id_product => params[:product]).where(:price_date => { '$gte' => fecha_inicial, '$lt' => fecha_final }, :currency => Currency.find(params[:currency]).name).ascending(:price_date)
-    #   @pricepoints = PricePoint.where(:id_product => { '$in' => params[:product] }, :price_date => { '$gte' => fecha_inicial, '$lt' => fecha_final }, :currency => Currency.find(params[:currency]).name).ascending(:price_date)
-    #   render :json => @pricepoints, :callback => params[:callback]
-    #   return
-    # end
+      @pricepoints = PricePoint.any_in(:id_product => params[:product]).where(:price_date => { '$gte' => fecha_inicial, '$lt' => fecha_final }, :currency => Currency.find(params[:currency]).name).ascending(:price_date)
+      @pricepoints = PricePoint.where(:id_product => { '$in' => params[:product] }, :price_date => { '$gte' => fecha_inicial, '$lt' => fecha_final }, :currency => Currency.find(params[:currency]).name).ascending(:price_date)
+      render :json => @pricepoints, :callback => params[:callback]
+      return
+    end
 
     where = Hash.new
     join = Array.new
